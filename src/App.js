378421ce25b59ede3104e15eca6595d198e7d02b
Zineblbr <117504309+Zineblbr@users.noreply.github.com>
2023-02-12 15:56:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: "Zineb LOUBAR",
+      bio: "I am a web developer",
+      imgSrc: "./Images/ProfilePhoto.png",
+      profession: "Freelancer",
+    },
+    show: false,
+  };
+
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="containerStyle">
+        <div className="card cardStyle">
+          <button
+            className="btn btn-primary btn-lg my-3 "
+            onClick={this.toggleShow}
+          >
+            Toggle Show
+          </button>
+          {this.state.show ? (
+            <div>
+              <img
+                className="rounded-circle profilePhoto"
+                src={this.state.person.imgSrc}
+                alt="person"
+              />
+              <h3>{this.state.person.fullName}</h3>
+              <p>{this.state.person.bio}</p>
+              <p>Profession: {this.state.person.profession}</p>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
